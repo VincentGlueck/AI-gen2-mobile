39e83926234f8fdf5a8429e1d08bc94f0897206e
Martin Lange <martin.h.lange@gmail.com>
2023-07-02 12:46:51 +0200

    * add missing files to GIT

---
+++ app/src/main/java/org/ww/ai/activity/ReceiveImage.java
@@ -0,0 +1,16 @@
+package org.ww.ai.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ww.ai.R;
+
+public class ReceiveImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_image);
+    }
+}
